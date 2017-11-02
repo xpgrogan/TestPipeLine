ced0b4819d07b96f729d0a11e20ee8a9157f9518
Xavier Grogan <xavier.grogan@aol.com>
2017-11-02 13:19:02 -0400

    Demo Ready?

--- src/jClaretyProject/java/jClaretyProject.java
+++ src/jClaretyProject/java/jClaretyProject.java
@@ -3,6 +3,5 @@ public class jClaretyProject
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Clarety Demo.");
-        System.out.println("Dev-Ops is helpful!");
     }
 }
